e331044a6b4b50f1ee0d1c758c4f531f3edd7404
heejin31 <109449854+heejin31@users.noreply.github.com>
2022-10-20 12:09:40 +0900

    Add files via upload

--- js/sub.js
+++ js/sub.js
@@ -33,4 +33,38 @@ $(document).ready(function(){
           }        
         ]
       });
+      $('#middle_slider_dining_sub').slick({
+        dots:true,
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1160,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 430,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }        
+        ]
+      });
 });
